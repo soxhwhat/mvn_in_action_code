bf0fcbe42515a11d984663872d1b6fe043fa4b5d
jiahui.huang <jiahui.huang@juphoon.com>
2022-11-24 14:04:42 +0800

    feature: 利用Archetype快速创建项目骨架

---
+++ maven-test/src/main/java/com/soxhwhat/mvnbook/Main.java
@@ -0,0 +1,7 @@
+package com.soxhwhat.mvnbook;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ maven-test/src/test/java/com/soxhwhat/mvnbook/MainTest.java
@@ -0,0 +1,15 @@
+package com.soxhwhat.mvnbook;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void testMain() {
+        // Setup
+        // Run the test
+        Main.main(new String[]{"args"});
+
+        // Verify the results
+    }
+}
